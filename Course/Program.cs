e9478ba8b58cc0a1a47520ac6a146b2927d83c0d
Felipe Tadeu Geraldo <felipe.develop@gmail.com>
2019-03-18 15:11:29 -0300

    exercicio polimorfismo

---
+++ Course/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Product> list = new List<Product>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'i' || ch == 'I')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    ImportedProduct ip = new ImportedProduct(productName, productPrice, customsFee);
+                    list.Add(ip);
+
+                }
+                else if (ch == 'u' || ch == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    UsedProduct up = new UsedProduct(productName, productPrice, manufactureDate);
+                    list.Add(up);
+                }
+                else
+                {
+                    Product p = new Product(productName, productPrice);
+                    list.Add(p);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p.PriceTag());
+            }
+        }
+    }
+}
